6916b88e5390c0349739f30be35f40b20e49ac0f
Jeff Zohrab <jzohrab@gmail.com>
2020-02-12 22:39:06 -0600

    Always test troublesome notes

--- find_note.rb
+++ find_note.rb
@@ -1,6 +1,8 @@
 # Command line, says string number from 1 (low E) to 6 (high E),
 # and then the note to find on that string.
 
+require 'yaml'
+
 class Array
   def shuffle!
     size.downto(1) { |n| push delete_at(rand(n)) }
@@ -46,13 +48,17 @@ end
 
 ###################################
 
+puts "Reading base file"
+h = YAML.load_file('find_note.yaml')
+persistent = h[:persistent].map do |q|
+  Question.new(q[:string], q[:note])
+end
+# puts persistent.inspect
+
 print "Enter notes to study: "
 NOTES = gets.split(' ')
 print "Enter strings: "
 STRINGS = gets.split(' ')
-
-Voice.say("Get ready!")
-Kernel.sleep 3
 
 questions = []
 NOTES.each do |n|
@@ -60,7 +66,11 @@ NOTES.each do |n|
     3.times { |i| questions << Question.new(s, n) }
   end
 end
+questions += persistent * 3
 questions.shuffle!
+
+Voice.say("Get ready!")
+Kernel.sleep 3
 
 n = 0
 questions.each do |q|
